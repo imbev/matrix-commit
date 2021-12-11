708dd099505858769613d6a5c68ebbe1a1fee029
krazykirby99999 <krazykirby99999@gmail.com>
2021-12-11 14:48:08 -0600

    Add details of commit to message sent

--- src/action.py
+++ src/action.py
@@ -1,11 +1,15 @@
 import os
+import time
 import simplematrixbotlib as botlib
+import git
 
 HOMESERVER = os.getenv("HOMESERVER")
 USERNAME = os.getenv("USERNAME")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ROOM_ID = os.getenv("ROOM_ID")
-MESSAGE = os.getenv("MESSAGE")
+MESSAGE = os.getenv("MESSAGE") or "Commit:"
+
+commit = git.Repo(".").head.commit
 
 creds = botlib.Creds(homeserver=HOMESERVER, username=USERNAME, access_token=ACCESS_TOKEN)
 bot = botlib.Bot(creds=creds)
@@ -15,9 +19,16 @@ async def send_message(joined_room_id: str) -> None:
     if ROOM_ID and ROOM_ID != joined_room_id:
         return
 
+    message = f"""
+{MESSAGE}
+Author: {commit.author.name}
+Timestamp: {time.asctime(time.gmtime(commit.committed_date))}
+Message: {commit.message}
+"""
+
     await bot.api.send_markdown_message(
         room_id=joined_room_id, 
-        message=MESSAGE, 
+        message=message, 
         msgtype="m.notice")
         
     exit()
