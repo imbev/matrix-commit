67d75893a638958359e592bcadaf45f4e9c749cb
krazykirby99999 <krazykirby99999@gmail.com>
2021-12-11 13:57:38 -0600

    Add bot that sends message to room on action run

--- src/action.py
+++ src/action.py
@@ -1,13 +1,27 @@
 import os
+import simplematrixbotlib as botlib
 
-homeserver = os.getenv("HOMESERVER")
-username = os.getenv("USERNAME")
-access_token = os.getenv("ACCESS_TOKEN")
-room_id = os.getenv("ROOM_ID")
-message = os.getenv("MESSAGE")
+HOMESERVER = os.getenv("HOMESERVER")
+USERNAME = os.getenv("USERNAME")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ROOM_ID = os.getenv("ROOM_ID")
+MESSAGE = os.getenv("MESSAGE")
 
-print(homeserver)
-print(username)
-print(access_token)
-print(room_id)
-print(message)
+print(MESSAGE)
+
+creds = botlib.Creds(homeserver=HOMESERVER, username=USERNAME, access_token=ACCESS_TOKEN)
+bot = botlib.Bot(creds=creds)
+
+@bot.listener.on_startup
+async def send_message(joined_room_id: str) -> None:
+    if ROOM_ID and ROOM_ID != joined_room_id:
+        return
+
+    await bot.api.send_markdown_message(
+        room_id=joined_room_id, 
+        message=MESSAGE, 
+        msgtype="m.notice")
+        
+    exit()
+
+bot.run()
